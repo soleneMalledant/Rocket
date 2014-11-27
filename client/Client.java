97d91512f7ab85e9e04e70547a2609c95441ba1d
Xavier Leblond <xavier.leblond@ymail.com>
2014-11-27 23:26:42 +0100

    some changes

--- client/Client.java
+++ client/Client.java
@@ -10,26 +10,6 @@ class Client {
 	public static void main(String args[]) throws IOException {
 		int port = (args.length < 1) ? Global.DEFAULT_PORT : Integer.parseInt(args[0]);
 		
-		String chaine = "";
-		
-		String filePath = args[0];
-		File fileToSend = new File(filePath);
-		if (fileToSend.isFile()) {
-			InputStream ips = new FileInputStream(fileToSend);
-			InputStreamReader ipr = new InputStreamReader(ips);
-			BufferedReader br = new BufferedReader(ipr);
-			String line;
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-				chaine += line + "\n";
-			}
-			br.close();
-		}
-		
-		
-		
-
-		
 		// SEND INFO
 		Socket soc = null;
 		BufferedReader br = null;
@@ -41,14 +21,37 @@ class Client {
 			br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 			dos = new DataOutputStream(soc.getOutputStream());
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 			System.exit(0);
 		}
+
+		String chaine = "";
+	    // READ FILE	
+		String filePath = args[1];
+        System.out.println(filePath);
+		File fileToSend = new File(filePath);
+		if (fileToSend.isFile()) {
+			InputStream ips = new FileInputStream(fileToSend);
+			InputStreamReader ipr = new InputStreamReader(ips);
+			BufferedReader breader = new BufferedReader(ipr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				chaine += line + "\n";
+                dos.writeBytes(line);
+                dos.flush();
+			}
+			breader.close();
+		}
+        
+
+
+        /*
 
 		System.out.println("Welcome message:" + br.readLine());
 		String incomingMessage = "";
 		String outgoingMessage = "";
-		do{
+		do {
 			outgoingMessage = userInput.readLine();
 			dos.writeBytes(outgoingMessage);
 			dos.write(13);
@@ -56,7 +59,9 @@ class Client {
 			dos.flush();
 			incomingMessage = br.readLine();
 			System.out.println("From server :" + incomingMessage);
-		}while(!(Global.END_CONNECTION.equals(outgoingMessage)));
+		} while(!(Global.END_CONNECTION.equals(outgoingMessage)));
+
+        */
 
 		br.close();
 		dos.close();
