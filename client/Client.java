57dd5c609a82de7fed1886a6e6b1f0d78bc9a37a
Xavier Leblond <xavier.leblond@ymail.com>
2014-12-02 13:56:19 +0100

    clean code and fix usage

--- client/Client.java
+++ client/Client.java
@@ -13,7 +13,7 @@ class Client {
     private Socket socket = null;
 
     public static void usage() {
-        System.out.println("TOCOMPLETE");
+        System.out.println("Please add a file to upload.");
     }
 
     public static void main(String args[]) throws IOException {
@@ -44,8 +44,6 @@ class Client {
         String filePath = args[0];
 
         File fileToSend = new File(filePath);
-//        dos.writeBytes("Prepare to write");
- //       dos.flush();
         if (fileToSend.isFile()) {
             if(oos == null) {
                 oos = new ObjectOutputStream(socket.getOutputStream());
@@ -55,48 +53,14 @@ class Client {
             oos.flush();
             
             // Send object file to the server.
-        
             oos.writeObject(fileToSend);
             oos.flush();
-            //sendFile(fileToSend);
             System.out.println("FILE SEND!!");
-
-
         }
-        //oos.writeBytes("EOF");
         
-        //oos.flush();
-        //br.close();
-
         // Close all streams.
         oos.close();
-        //dos.close();
         socket.close();
     }
 
-    public static void sendFile(File file) {
-
-        /*try { 
-            InputStream ips = new FileInputStream(file);
-            InputStreamReader ipr = new InputStreamReader(ips);
-            BufferedReader breader = new BufferedReader(ipr);
-            String line;
-            System.out.println("File uploading...");
-            while ((line = breader.readLine()) != null) {
-                dos.writeBytes(line + '\n');
-                dos.flush();
-            }
-            breader.close();
-            ips.close();
-            ipr.close();
-            dos.writeBytes("EOF");
-            dos.flush();
-
-        } catch (IOException ioe) {
-            System.err.println(ioe.getMessage());
-            ioe.getStackTrace();
-        }
-        System.out.println("File uploaded!!");*/
-
-    }
 }
