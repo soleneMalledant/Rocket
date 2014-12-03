d22cda22bb5d30b59334e1d767ca6006aa9ffdd8
Xavier Leblond <xavier.leblond@ymail.com>
2014-12-03 13:17:46 +0100

    add with ssl

---
+++ withssl/client/Client.java
@@ -0,0 +1,118 @@
+package client;
+
+import java.net.*;
+import java.io.*;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+
+import util.Global;
+
+class Client {
+
+    private BufferedReader br = null;
+    private static DataOutputStream dos = null;
+    private Socket socket = null;
+
+    public static void usage() {
+        System.out.println("Usage: Client COMMAND <filename>");
+        System.out.println("Commands: upload|download");
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        String option = null; 
+        if (args.length <= 1) {
+            usage();
+            System.exit(0);
+        } else if  (("upload".equals(args[0]) || ("download".equals(args[0]))))  {
+            option = args[0]; 
+        } else {
+            usage();
+            System.exit(0);
+        }
+
+        //int port = (args.length < 1) ? Global.DEFAULT_PORT : Integer.parseInt(args[0]);
+        int port = Global.DEFAULT_PORT;
+
+
+
+        // SEND INFO
+        Socket socket = null;
+        BufferedReader br = null;
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        ObjectOutputStream oos = null; 
+        ObjectInputStream ois = null;
+
+
+        try {
+            socket = new Socket(InetAddress.getLocalHost(), port, InetAddress.getLocalHost(), Global.RANDOM_PORT);
+            dos = new DataOutputStream(socket.getOutputStream());
+            System.out.println("CONNECTION ESTABLISHED...");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+
+
+        if(option.equals("upload")) {
+
+
+            String filePath = args[1];
+            File fileToSend = new File(filePath);
+            if (fileToSend.isFile()) {
+                if(oos == null) {
+                    oos = new ObjectOutputStream(socket.getOutputStream());
+                }
+
+                oos.writeObject("RECEIVE_FILE@@");
+                oos.flush();
+
+                // Send object file to the server.
+                oos.writeObject(fileToSend);
+                oos.flush();
+                System.out.println("FILE SEND!!");
+            }
+
+        } else if (option.equals("download")) {
+            if(oos == null) {
+                oos = new ObjectOutputStream(socket.getOutputStream());
+            }
+            System.out.println("debug: downloading file");
+            oos.writeObject("SEND_FILE@@" + args[1]);
+            oos.flush();
+            if (ois == null)  {
+                ois =  new ObjectInputStream(socket.getInputStream()); 
+            }
+            try {
+                File file = (File) ois.readObject();
+                String filePathToWrite = file.getName();
+                File fileToWrite = new File(filePathToWrite);
+                Files.copy(file.toPath(), fileToWrite.toPath());
+
+            } catch (Exception e){
+                System.err.println(e.getMessage());
+            }
+        }
+
+        // Close all streams.
+        if (ois != null) {
+            ois.close();
+        }
+        if (oos != null) {
+            oos.close();
+        }
+
+        socket.close();
+
+    }     
+
+
+
+
+}
+
+
+
+
---
+++ withssl/server/ClientHandler.java
@@ -0,0 +1,113 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.io.ObjectInputStream;
+import java.io.File;
+import java.nio.file.Files;
+
+import util.Global;
+
+
+public class ClientHandler implements Runnable {
+    private BufferedReader br = null;
+    private DataOutputStream dos = null;
+    private Socket socket = null;
+    private ObjectInputStream ois = null;
+    private ObjectOutputStream oos = null;
+    private int id = 0;//Remote port, two different clients could have the same though!
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.br = new BufferedReader(new InputStreamReader(socket.getInputStream()));;
+        this.dos = new DataOutputStream(socket.getOutputStream());
+        this.socket = socket;
+        this.id  = this.socket.getPort();//This ID might not be unique!
+        this.ois = new ObjectInputStream(socket.getInputStream());
+        //	this.sendWelcomeMessage();
+        System.out.println("++ #" + String.format("%05d", this.id));
+    }
+
+    private void sendWelcomeMessage() throws IOException {
+        this.dos.write(new String("Write \"" + Global.END_CONNECTION + "\" to disconnect").getBytes());
+        this.dos.write('\r');//send LF(0xa): Line Feed
+        this.dos.write('\n');//send CR(0xd): Carriage Return
+        this.dos.flush();
+    }
+
+    @Override
+    public void run() {
+        try {
+            String incomingMessage = null;
+            String[] message = null;
+            boolean quit = false;
+
+            do {
+
+                try {
+                    incomingMessage = (String) ois.readObject();
+                    message = incomingMessage.split("@@");
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
+
+
+
+
+                if("RECEIVE_FILE@@".equals(incomingMessage)) {
+                    try {
+                        if (ois == null) {
+                            ois = new ObjectInputStream(socket.getInputStream());
+                        }
+                        File file = (File) ois.readObject();
+                        String filePathToWrite = Global.SERVER_REPOSITORY + "/" + file.getName();
+                        File fileToWrite = new File(filePathToWrite);
+                        Files.copy(file.toPath(), fileToWrite.toPath());
+                        quit = true;
+                        System.out.println("File receive!");
+                    } catch (ClassNotFoundException cnfe) {
+                        System.err.println(cnfe.getMessage());
+                        cnfe.printStackTrace();
+                    } catch (Exception e) {
+                        System.err.println(e);
+                        e.printStackTrace();
+                        System.exit(0);
+
+                    }
+
+                } else if ("SEND_FILE".equals(message[0])) {
+                    File fileToSend = new File(Global.SERVER_REPOSITORY + "/" + message[1]);
+                    if(oos == null) {
+                        oos = new ObjectOutputStream(socket.getOutputStream());
+                    }
+                    oos.writeObject(fileToSend);
+                    oos.flush();
+                    quit = true; 
+                    System.out.println("File send to client");
+                }
+
+
+            } while (!quit);
+
+            System.out.println("Bye Bye");
+
+            // Close all streams.
+
+            if(ois != null) {
+                this.ois.close();
+            }
+            if (oos != null) {
+                this.oos.close();
+            }
+            this.socket.close();
+
+            System.out.println("-- #" + String.format("%05d", this.id));
+        } catch (IOException e) {
+            System.err.println("Error occurred:" + e.getMessage());
+        }
+    }
+
+}
